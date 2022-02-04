46fc8093fb80e0f56526fe78bb998d698171280d
rafaeljcamara <rafaelcamarac@gmail.com>
2022-02-04 23:13:23 +0000

    Minor bug fix in auth routes return.

--- routes/auth.js
+++ routes/auth.js
@@ -17,9 +17,9 @@ router.post("/register", async (req, res) => {
         });
         //save user in database
         const savedUser = await newUser.save();
-        res.status(200).json(savedUser);
+        return res.status(200).json(savedUser);
     } catch (err) {
-        res.status(500).json(err);
+        return res.status(500).json(err);
     }
 });
 
@@ -30,20 +30,20 @@ router.post("/login", async (req, res) => {
 
         //check if email exists
         const foundUser = await User.findOne({ email });
-        if (!foundUser) res.status(404).json({
+        if (!foundUser) return res.status(404).json({
             message: "Error! No such user was found."
         });
 
         //if email exists check if passwords are matching
         const validPassword = await bcrypt.compare(password, foundUser.password);
-        if (!validPassword) res.status(400).json({
+        if (!validPassword) return res.status(400).json({
             message: "Error! Passwords are not matching."
         });
 
-        res.status(200).json(foundUser);
+        return res.status(200).json(foundUser);
 
     } catch (err) {
-        res.status(500).json(err);
+        return res.status(500).json(err);
     }
 });
 
